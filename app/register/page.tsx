3059c37c4b472a89c425d36b681895268979210c
Arthur Fernandez <arthur@snooget.com>
2023-09-20 05:48:03 -0700

    created register page

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+const registerPage = () => {
+  return (
+    <div>
+      <div>Register For Store Here</div>
+      <div>Ocomni.com Register form will be imported</div>
+    </div>
+  );
+};
+
+export default registerPage;
