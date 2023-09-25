01825c611070213d3a22fb3fb3cfa75de2bb5fc4
Arthur Fernandez <arthur@snooget.com>
2023-09-24 18:11:44 -0700

    Made registration its own proper client component

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,157 +1,80 @@
-"use client";
-
 // React Imports
-import React, { useState, ChangeEvent, FormEvent } from "react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-// Firebase imports
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  updateProfile,
-} from "firebase/auth";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore";
-import { db } from "@/firebaseConfig";
-// React Icons Imports
-import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+// import React, { useState, ChangeEvent, FormEvent } from "react";
+// import Link from "next/link";
+// import { useRouter } from "next/navigation";
+// // Firebase imports
+// // import {
+// //   getAuth,
+// //   createUserWithEmailAndPassword,
+// //   updateProfile,
+// // } from "firebase/auth";
+// import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+// import { db } from "@/firebaseConfig";
+// // React Icons Imports
+// import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import RegisterUi from "@/components/ui/register-ui";
 
 // import { toast } from "react-toastify";
 
-// Below I am defining the type for the pages state...
-interface FormData {
-  email: string;
-  password?: string;
-  name: string;
-  timestamp?: any;
-}
+// // Below I am defining the type for the pages state...
+// interface FormData {
+//   email: string;
+//   password?: string;
+//   name: string;
+//   timestamp?: any;
+// }
 
 export default function Register() {
-  const router = useRouter();
-  // Below I added <boolean> for typescript saftey
-  const [showPassword, setShowPassword] = useState<boolean>(false);
+  // const router = useRouter();
+  // // Below I added <boolean> for typescript saftey
+  // const [showPassword, setShowPassword] = useState<boolean>(false);
 
-  // this was changed to <FormData> which is part of the interface declaration...
-  const [formData, setFormData] = useState<FormData>({
-    email: "",
-    password: "",
-    name: "",
-  });
+  // // this was changed to <FormData> which is part of the interface declaration...
+  // const [formData, setFormData] = useState<FormData>({
+  //   email: "",
+  //   password: "",
+  //   name: "",
+  // });
 
-  const { email, password, name } = formData;
-  // Added ChangeEvent, and <HTMLInputElement> for more??? I gotta get more review here in the onChange function
-  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  }
-  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+  // const { email, password, name } = formData;
+  // // Added ChangeEvent, and <HTMLInputElement> for more??? I gotta get more review here in the onChange function
+  // function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+  //   setFormData((prevState) => ({
+  //     ...prevState,
+  //     [e.target.id]: e.target.value,
+  //   }));
+  // }
+  // async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault();
 
-    try {
-      const auth = getAuth();
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password!
-      );
+  //   try {
+  //     const auth = getAuth();
+  //     const userCredential = await createUserWithEmailAndPassword(
+  //       auth,
+  //       email,
+  //       password!
+  //     );
 
-      const user = userCredential.user;
-      if (user) {
-        await updateProfile(user, {
-          displayName: name,
-        });
-      }
-      const formDataCopy = { ...formData };
-      delete formDataCopy.password;
-      formDataCopy.timestamp = serverTimestamp();
+  //     const user = userCredential.user;
+  //     if (user) {
+  //       await updateProfile(user, {
+  //         displayName: name,
+  //       });
+  //     }
+  //     const formDataCopy = { ...formData };
+  //     delete formDataCopy.password;
+  //     formDataCopy.timestamp = serverTimestamp();
 
-      await setDoc(doc(db, "users", user.uid), formDataCopy);
-      router.push("https://www.ocomni.com/account");
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  //     await setDoc(doc(db, "users", user.uid), formDataCopy);
+  //     router.push("https://www.ocomni.com/account");
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   return (
     <>
-      {/* Disclaimer Will need to change UI to support the template selected from the previous page.*/}
-      <section className="mt-8">
-        <div className="pl-8 pr-2 lg:text-center">
-          <div className="text-slate-200 text-4xl font-semibold mb-4">
-            <h1>Register for an account</h1>
-          </div>
-          <div className="text-slate-400">
-            <p className="lg:text-lg">
-              We'll email you your template and give instructions on how to
-              login!
-            </p>
-          </div>
-        </div>
-      </section>
-      <section className="mx-auto my-14 max-w-6xl px-8 h-vh">
-        <div className="my-8 max-w-[300px]  mx-auto">
-          <div className="flex flex-row items-center ">
-            <div className="cursor-pointer border-b-2 border-purple-600 w-full">
-              <Link href="/register">
-                <h3 className="uppercase font-bold text-xs text-center mb-3 tracking-wider">
-                  register
-                </h3>
-              </Link>
-            </div>
-          </div>
-          <div>
-            <form onSubmit={onSubmit}>
-              <div className="flex flex-col gap-3 mt-4">
-                <input
-                  type="email"
-                  id="email"
-                  onChange={onChange}
-                  value={email}
-                  className="pr-4 pl- py-3 w-full border-slate-400 border-[1px]  transition duration-150 rounded-sm text-sm bg-inherit focus:ring-purple-600 hover:border-purple-600 active:border-purple-900"
-                  placeholder=" Email"
-                />
-                <div className="bg-inherit relative">
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    id="password"
-                    value={password}
-                    onChange={onChange}
-                    className="pr-4 pl- py-3 w-full border-slate-400 border-[1px]  transition duration-150 rounded-sm text-sm bg-inherit focus:ring-purple-600 hover:border-purple-600 active:border-purple-900"
-                    placeholder=" Your password"
-                  />
-                  {showPassword ? (
-                    <AiFillEyeInvisible
-                      className="absolute right-3 top-3  text-2xl text-gray-700 cursor-pointer"
-                      onClick={() => setShowPassword((prevState) => !prevState)}
-                    />
-                  ) : (
-                    <AiFillEye
-                      className="absolute right-3 top-3 text-2xl text-gray-700 cursor-pointer"
-                      onClick={() => setShowPassword((prevState) => !prevState)}
-                    />
-                  )}
-                </div>
-                <input
-                  type="text"
-                  id="name"
-                  onChange={onChange}
-                  value={name}
-                  className="pr-4 pl- py-3 w-full border-slate-400 border-[1px]  transition duration-150 rounded-sm text-sm bg-inherit focus:ring-purple-600 hover:border-purple-600 active:border-purple-900"
-                  placeholder=" Enter your name"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full mt-8 text px-7 py-3 font-medium text-sm rounded-sm bg-[#6366F1] text-white hover:text-white overflow-hidden transition ease-in-out duration-150 hover:bg-[#5F56D6] active:bg-[#5B45BB] "
-              >
-                <Link href="/thankyou">Continue</Link>
-                {" >"}
-              </button>
-            </form>
-          </div>
-        </div>
-      </section>
+      <RegisterUi></RegisterUi>
     </>
   );
 }
---
+++ components/ui/register-ui.tsx
@@ -0,0 +1,73 @@
+"use client";
+// React Imports
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+//Firebase imports
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+
+// React Icons Imports
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+
+// Below I am defining the type for the pages state...
+interface FormData {
+  email: string;
+  password?: string;
+  name: string;
+  timestamp?: any;
+}
+
+export default function RegisterUi() {
+  const router = useRouter();
+  // Below I added <boolean> for typescript saftey
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  // this was changed to <FormData> which is part of the interface declaration...
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  const { email, password, name } = formData;
+  // Added ChangeEvent, and <HTMLInputElement> for more??? I gotta get more review here in the onChange function
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password!
+      );
+
+      const user = userCredential.user;
+      if (user) {
+        await updateProfile(user, {
+          displayName: name,
+        });
+      }
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+      router.push("https://www.ocomni.com/account");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
